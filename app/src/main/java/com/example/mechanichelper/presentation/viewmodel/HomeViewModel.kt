7de5fd21de8a96d3e2ffc71e71fa2f6c3d7cc43e
onecripshama <onecripnigga@yandex.ru>
2025-03-10 20:49:57 +0300

    update screen after taking photo

--- app/src/main/java/com/example/mechanichelper/presentation/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/mechanichelper/presentation/viewmodel/HomeViewModel.kt
@@ -14,7 +14,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
-import java.io.File
 import javax.inject.Inject
 
 @HiltViewModel
